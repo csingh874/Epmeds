dbb0ecd7527b6d3dcba324aabec8c675beb97051
CJ <cj@planet-odoo.com>
2021-10-06 11:13:53 +0530

    Added Comment

--- custom/admin.py
+++ custom/admin.py
@@ -5,23 +5,23 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
-@admin.register(get_user_model())
-class CustomUserForm(UserAdmin):
-    model = get_user_model()
-
-    list_display = ("id", "username",)
-    readonly_fields = ("date_joined", "last_login")
-    fieldsets = ((None, {'fields': ('username', 'password',)}),
-                 ('Personal info', {'fields': ('account', 'first_name', 'last_name', 'middle_initiative', 'email', 'speciality', 'address', 'city',
-                                               'state', 'zip_code', 'country', 'telephone', 'rec_date')}),
-                 ('Doctor', {'fields': ('dr_grp', 'address2', 'city2', 'state2', 'zip_code2')}),
-                 ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-                 ('Important dates', {'fields': ('last_login', 'date_joined')}),
-                 )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
-        }),
-    )
+# @admin.register(get_user_model())
+# class CustomUserForm(UserAdmin):
+#     model = get_user_model()
+#
+#     list_display = ("id", "username",)
+#     readonly_fields = ("date_joined", "last_login")
+#     fieldsets = ((None, {'fields': ('username', 'password',)}),
+#                  ('Personal info', {'fields': ('account', 'first_name', 'last_name', 'middle_initiative', 'email', 'speciality', 'address', 'city',
+#                                                'state', 'zip_code', 'country', 'telephone', 'rec_date')}),
+#                  ('Doctor', {'fields': ('dr_grp', 'address2', 'city2', 'state2', 'zip_code2')}),
+#                  ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+#                  ('Important dates', {'fields': ('last_login', 'date_joined')}),
+#                  )
+#
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('username', 'email', 'password1', 'password2'),
+#         }),
+#     )
--- custom/urls.py
+++ custom/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 app_name = 'custom'
 urlpatterns = [
-    path('', views.home_view),
-    path('services/', views.services_view),
-    path('what_we_do/', views.what_we_do),
-    path('become_member/', views.become_member),
-    path('career/', views.career),
-    path('contact_us/', views.contact_us),
-    path('logout/', views.user_logout, name='user_logout'),
-    re_path(r'^signup/(provider|member)$', views.register_mem_prov, name="user_signup"),
-    re_path(r'^login/(provider|member)$', views.user_login, name='user_login'),
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
-    path('change-password/<idx>', views.verify_otp, name='verify_otp'),
+    # path('', views.home_view),
+    # path('services/', views.services_view),
+    # path('what_we_do/', views.what_we_do),
+    # path('become_member/', views.become_member),
+    # path('career/', views.career),
+    # path('contact_us/', views.contact_us),
+    # path('logout/', views.user_logout, name='user_logout'),
+    # re_path(r'^signup/(provider|member)$', views.register_mem_prov, name="user_signup"),
+    # re_path(r'^login/(provider|member)$', views.user_login, name='user_login'),
+    # path('forgot_password/', views.forgot_password, name='forgot_password'),
+    # path('change-password/<idx>', views.verify_otp, name='verify_otp'),
 ]
